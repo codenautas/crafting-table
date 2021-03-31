3daebec18951ae451284f66b783607d3778d2e5d
Emilio Platzer <emilioplatzer@gmail.com>
2021-03-31 11:44:51 -0300

    3 pruebsa de controlarNiveles

--- src/test/test.ts
+++ src/test/test.ts
@@ -21,11 +21,37 @@ console.log(!!compareFiles)
 describe('controlarNiveles', function(){
     it('ok de 2 niveles', async function(){
         var tablaOk = [
-            {nivel:1, gasto:1234},
-            {nivel:2, gasto: 234},
-            {nivel:2, gasto:1000},
+            {nivel:0, gasto:1234},
+            {nivel:1, gasto: 234},
+            {nivel:1, gasto:1000},
         ];
         var {result} = controlarNiveles(tablaOk, 'nivel', 'gasto');
         assert.equal(result, true);
+    });
+    it('no suma de 2 niveles', async function(){
+        var tablaOk = [
+            {nivel:0, gasto:1234},
+            {nivel:1, gasto: 234},
+            {nivel:1, gasto: 777},
+        ];
+        var {result} = controlarNiveles(tablaOk, 'nivel', 'gasto');
+        assert.equal(result, false);
+    });
+    it('no suma con 4 niveles', async function(){
+        var tablaOk = [
+            {nivel:0, gasto:1234},
+            {nivel:1, gasto: 234},
+            {nivel:1, gasto:1000},
+            {nivel:2, gasto: 234},
+            {nivel:2, gasto: 750},
+            {nivel:2, gasto: 250},
+            {nivel:3, gasto: 234},
+            {nivel:3, gasto: 500},
+            {nivel:3, gasto: 100},
+            {nivel:3, gasto: 250},
+        ];
+        var {result, error} = controlarNiveles(tablaOk, 'nivel', 'gasto');
+        assert.equal(result, false);
+        assert.equal(error, "el nivel 3 debía sumar 1234 y suma 1084");
     });
 });
--- src/tool/pre-tools.ts
+++ src/tool/pre-tools.ts
@@ -3,5 +3,23 @@
 export type Record<T extends string> = {[x in T]: any}
 
 export function controlarNiveles<Keys extends string>(tabla:Record<Keys>[], columnaGrupo:Keys, columnaControlar:Keys){
-    return {result:tabla[0][columnaGrupo] != tabla[0][columnaControlar]};
+    var sumadores:number[] = []
+    for(var row of tabla){
+        var valorAControlar = row[columnaControlar];
+        var grupoDelaRow = row[columnaGrupo];
+        if(sumadores[grupoDelaRow] == null){
+            sumadores[grupoDelaRow] = 0;
+        }
+        sumadores[grupoDelaRow] = sumadores[grupoDelaRow] + valorAControlar;
+    }
+    var sumaNivel0 = sumadores[0];
+    for(var nivelActual in sumadores){
+        var sumaEsteNivel = sumadores[nivelActual];
+        if(sumaEsteNivel != sumaNivel0){
+            return {result:false, error:'el nivel '+nivelActual+' debía sumar '+sumaNivel0+' y suma '+sumaEsteNivel}
+        }
+    }
+    return {
+        result:true
+    };
 }
